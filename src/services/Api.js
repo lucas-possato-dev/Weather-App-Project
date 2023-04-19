c73503b5aa50ab0ff5b4bc84b0745b759372ad2e
lucas-possato-dev <lucaspstinho@gmail.com>
2023-04-18 23:58:34 -0300

    https add to link api

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 const KEY = '284286d903074f57b9505421231904';
 
 const fetchData = async (city) => {
-const url = `http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
+const url = `https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
 
 const response = await fetch(url);
 const data = await response.json();
